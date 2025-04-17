29ea1445c3e5969289e0d9c10a219da23db91401
girish-n-com <girikripa@yahoo.com>
2025-04-17 22:12:27 +0200

    updated the vertifications and badges images

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -3,26 +3,11 @@ import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png"
 
 
-import pythonImage from "../assets/img/python-svgrepo-com_new.svg"
-import dockerImage from "../assets/img/docker-svgrepo-com_new.svg"
-import azureImage from "../assets/img/microsoftazure-svgrepo-com.svg"
-import devopsImage from "../assets/img/azure-devops-svgrepo-com.svg"
-import githubImage from "../assets/img/github-142-svgrepo-com.svg"
-import djangoImage from "../assets/img/django-svgrepo-com.svg"
-import flaskImage from "../assets/img/flask-svgrepo-com.svg"
-import airflowImage from "../assets/img/airflow-svgrepo-com.svg"
-import linuxImage from "../assets/img/linux-svgrepo-com.svg"
-import pandasImage from "../assets/img/pandas-svgrepo-com.svg"
-import jenkinsImage from "../assets/img/jenkins-svgrepo-com.svg"
-import nginxImage from "../assets/img/nginx-svgrepo-com.svg"
-import javascriptImage from "../assets/img/javascript-svgrepo-com.svg"
-import postgresImage from "../assets/img/postgresql-svgrepo-com.svg"
-import reactImage from "../assets/img/react-svgrepo-com.svg"
-import seleniumImage from "../assets/img/selenium-svgrepo-com.svg"
-import sqliteImage from "../assets/img/sqlite-svgrepo-com.svg"
-import bambooImage from "../assets/img/bamboo-svgrepo-com.svg"
-import androidImage from "../assets/img/android-color-svgrepo-com.svg"
-import cmdImage from "../assets/img/command-line-svgrepo-com.svg"
+import microsoftDataFun from "../assets/img/microsoft_data_fundamentals_sym.jpg"
+import istqbFoundation from "../assets/img/istqb_foundation_level_sym.jpg"
+import databricksDataFun from "../assets/img/databricks_data_fundamentals_sym.jpg"
+import databricksGenAIFun from "../assets/img/databricks_generative_ai_sym.jpg"
+import liquibaseFun from "../assets/img/liquibase_fundamentals_sym.jpg"
 
 export const Certification = () => {
   const responsive = {
@@ -54,64 +39,19 @@ export const Certification = () => {
                         <p></p>
                         <Carousel responsive={responsive} autoPlay={true} autoPlaySpeed={2000} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={pythonImage} alt="Image" />
+                                <img src={microsoftDataFun} alt="Image" />
                             </div>
                             <div className="item">
-                                <img src={dockerImage} alt="Image" />
+                                <img src={istqbFoundation} alt="Image" />
                             </div>
                             <div className="item">
-                                <img src={azureImage} alt="Image" />
+                                <img src={databricksDataFun} alt="Image" />
                             </div>
                             <div className="item">
-                                <img src={airflowImage} alt="Image" />
+                                <img src={databricksGenAIFun} alt="Image" />
                             </div>
                             <div className="item">
-                                <img src={postgresImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={reactImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={devopsImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={githubImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={djangoImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={flaskImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={jenkinsImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={linuxImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={pandasImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={nginxImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={javascriptImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={sqliteImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={seleniumImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={bambooImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={androidImage} alt="Image" />
-                            </div>
-                            <div className="item">
-                                <img src={cmdImage} alt="Image" />
+                                <img src={liquibaseFun} alt="Image" />
                             </div>
                         </Carousel>
                     </div>
